3e0c397d1578b709fd1224c3453d3e2786d7081f
janthony25 <junanthony.business@gmail.com>
2024-02-18 23:20:57 +1300

    Add ApplicationDbContext

---
+++ MVC-Practice5/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC_Practice5.Data
+{
+    public class ApplicationDbContext
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
